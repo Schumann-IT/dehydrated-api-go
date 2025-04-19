218bc65302739d8e524818b521ead01cca91a149
Jan Schumann <js@schumann-it.com>
2025-04-19 18:54:38 +0200

    cleanup make targets

--- generate_test.go
+++
@@ -1,8 +0,0 @@
-//go:build testplugin
-// +build testplugin
-
-package main
-
-//go:generate echo "Building test plugin..."
-//go:generate go build -o internal/plugin/grpc/testdata/test-plugin/test-plugin internal/plugin/grpc/testdata/test-plugin/main.go
-//go:generate echo "test plugin generation complete!"
